e95fcc6ba7224754bb51cb86fc02b7e9cf8b9ccf
Micah Redding <micahtredding@gmail.com>
2016-09-05 16:34:06 -0500

    Add Chords and Lyrics markup processor and styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,37 @@
 module ApplicationHelper
+  class ChordsAndLyrics < Redcarpet::Render::HTML
+    def initialize(options={})
+      super options.merge(:hard_wrap => true)
+    end
+
+    def chord_markup(text)
+      chord_name = "[CDEFGAB](#|##|b|bb)?(m|maj7|maj|min7|min|sus2)?"
+      chord_name_multiple = "(#{chord_name}([ ])?)+"
+
+      chord_regex = /(?<chord>\[#{chord_name}\])/
+      chord_multiple_regex = /(?<chord>\[#{chord_name_multiple}\])/
+
+      bracket_regex = /(?<chord>\[.*?\])/
+      text.gsub(chord_multiple_regex, '<span class="chord">\k<chord></span>')
+    end
+
+    def hard_wrap(text)
+      # text.gsub(/(.*\n)/, '<span class="line-break">\1</span>' + "\n")
+      text.split(/\n/).map { |t| '<span class="line-break">' + t + '</span>' }.join()
+    end
+
+    def paragraph(t)
+      # a = hard_wrap(chord_markup(t))
+      b = chord_markup(hard_wrap(t))
+      text = b
+      "<p>#{text}</p>"
+    end
+  end
+
+
   def markdown_renderer
     @markdown_renderer ||= Redcarpet::Markdown.new(
-      Redcarpet::Render::HTML.new(
+      ChordsAndLyrics.new(
         :link_attributes => Hash["target" => "_blank"],
         :hard_wrap => true,
       ),
