7609c53993d86e5e33a0c86393b2683457eaa855
Micah Redding <micahtredding@gmail.com>
2016-09-07 10:02:00 -0500

    Add title field to tracks

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,8 +2,11 @@ class Track < ApplicationRecord
   belongs_to :release
   belongs_to :song
 
-  delegate  :title,
-            :audio_url,
+  delegate  :audio_url,
             :lyrics,
             :to => :song, :allow_nil => true
+
+  # def title
+  #   @song.present? ? @song.title : @title
+  # end
 end
